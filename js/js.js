5dd98ea6eb25acecb792c991af02d65c1833d4c2
Zakhar Shchipets <zshchipets@gmail.com>
2019-01-05 00:37:43 +0300

    +menu

--- js/js.js
+++ js/js.js
@@ -1,7 +1,13 @@
+//call popup
 $('.header__call-btn').click(function() {
   $('.contacts__popup').addClass('contacts__popup--visible');
 });
 
 $('.contacts__close').click(function() {
   $('.contacts__popup').removeClass('contacts__popup--visible');
+});
+
+//menu
+$('#menu').click(function() {
+  $('.menu__popup').toggleClass('menu__popup--visible');
 });
