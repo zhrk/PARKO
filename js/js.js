1ec7b9845630b25dfb82515e04cf332f7b3764b0
Zakhar Shchipets <zshchipets@gmail.com>
2019-04-27 15:17:16 +0300

    Add scroll up, fix header

--- js/js.js
+++ js/js.js
@@ -1,32 +1,38 @@
 //call popup
-$('.header__call-btn').click(function() {
-  $('.contacts__popup').addClass('contacts__popup--visible');
+$(".header__call-btn").click(function() {
+  $(".contacts__popup").addClass("contacts__popup--visible");
 });
 
-$('.contacts__close').click(function() {
-  $('.contacts__popup').removeClass('contacts__popup--visible');
+$(".contacts__close").click(function() {
+  $(".contacts__popup").removeClass("contacts__popup--visible");
 });
 
 //menu
-$('#menu, .header__hamburger').click(function() {
-	$('.header__hamburger, .menu__hamburger').toggleClass('active');
-  $('.menu__popup').toggleClass('menu__popup--visible');
-  $('.menu').toggleClass('menu--visible');
+$("#menu, .header__hamburger").click(function() {
+  $(".header__hamburger, .menu__hamburger").toggleClass("active");
+  $(".menu__popup").toggleClass("menu__popup--visible");
+  $(".menu").toggleClass("menu--visible");
 });
-$('.menu__close').click(function(){
-	$('.menu__popup').removeClass('menu__popup--visible');
-  $('.menu').removeClass('menu--visible');
-  $('.header__hamburger').removeClass('active');
+$(".menu__close").click(function() {
+  $(".menu__popup").removeClass("menu__popup--visible");
+  $(".menu").removeClass("menu--visible");
+  $(".header__hamburger").removeClass("active");
 });
-$('.menu__tabs-btn--services').click(function(){
-	$('.menu__tabs-btn').removeClass('active');
-	$(this).addClass('active');
-	$('.menu__popup').addClass('menu__services--visible');
-	$('.menu__popup').removeClass('menu__main--visible');
+$(".menu__tabs-btn--services").click(function() {
+  $(".menu__tabs-btn").removeClass("active");
+  $(this).addClass("active");
+  $(".menu__popup").addClass("menu__services--visible");
+  $(".menu__popup").removeClass("menu__main--visible");
 });
-$('.menu__tabs-btn--main').click(function(){
-	$('.menu__tabs-btn').removeClass('active');
-	$(this).addClass('active');
-	$('.menu__popup').removeClass('menu__services--visible');
-	$('.menu__popup').addClass('menu__main--visible');
+$(".menu__tabs-btn--main").click(function() {
+  $(".menu__tabs-btn").removeClass("active");
+  $(this).addClass("active");
+  $(".menu__popup").removeClass("menu__services--visible");
+  $(".menu__popup").addClass("menu__main--visible");
+});
+
+/* scroll-up */
+$(".scroll-up").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, 600);
+  return false;
 });
